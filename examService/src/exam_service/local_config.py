d63dcfb334c4bebdf95723ed0a0dc61e9592242e
TurkinDV <dmitry_turkin@mail.ru>
2022-03-14 23:13:00 +0300

    init commit exam service

---
+++ examService/src/exam_service/local_config.py
@@ -0,0 +1,21 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-h94biae8=hpd*vus_2xiryzrr0g+=p=x9dbc_^37befu4v4n+q'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ["*"]
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': 5432,
+    }
+}
